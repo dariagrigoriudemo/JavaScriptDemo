cc2830b7dc79a77a3c60f13e603ed979793e2773
Daria Grigoriu <dariagrigoriu@Darias-MBP.home>
2018-11-25 17:04:52 -0800

    Azure Pipelines script update

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -1,16 +1,37 @@
+function isPrime(value) {
+    if (value <= 1) {
+        return false;
+    } else {
+        for(var div = 2 ; div * div <= value; div++) {
+            if (value % div == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}   
+
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+    var value = (req.query.value || (req.body && req.body.value));
+    if (value) {
+        if (isNaN(value)) {
+            context.res = {
+                status: 400, 
+                body: "value is not a number : " + (value)
+            };
+        } 
+        else {
+            context.res = {
+                // status: 200, /* Defaults to 200 */
+                body: "value is prime number : " + (isPrime(value))
+            };
+        }
     }
     else {
         context.res = {
             status: 400,
-            body: "Please pass a name on the query string or in the request body"
+            body: "Please pass a value on the query string or in the request body"
         };
     }
 };
