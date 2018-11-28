7eb02e6a2577d7240029772c6bb8e642a8563ca1
Daria Grigoriu <dariac@ntdev.microsoft.com>
2018-11-28 00:22:21 -0800

    Queue function updated

--- QueueTrigger/index.js
+++ QueueTrigger/index.js
@@ -1,3 +1,27 @@
+function isPrime(value) {
+    if (value <= 1) {
+        return false;
+    } else {
+        for(var div = 2 ; div * div <= value; div++) {
+            if (value % div == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}   
+
 module.exports = async function (context, myQueueItem) {
     context.log('JavaScript queue trigger function processed work item', myQueueItem);
+    if (myQueueItem) {
+        if (isNaN(myQueueItem)) {
+            context.log("value is not a number : " + (myQueueItem));
+        } 
+        else {
+            context.log("The test value is prime number : " + (isPrime(myQueueItem)));
+        }
+    }
+    else {
+        context.log("null value for the queue input");
+    }
 };
