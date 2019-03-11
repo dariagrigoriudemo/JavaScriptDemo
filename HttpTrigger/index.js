1e1c739a1eff49bca3d02aaa17cf00aafd4bf4e6
dariagrigoriudemo <d.grigoriu@live.com>
2019-03-11 16:02:49 -0700

    Updated message

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -24,7 +24,7 @@ module.exports = async function (context, req) {
         else {
             context.res = {
                 // status: 200, /* Defaults to 200 */
-                body: "The prime number test result for ProDev All Hands is: " + (isPrime(value))
+                body: "The prime number test result is: " + (isPrime(value))
             };
         }
     }
